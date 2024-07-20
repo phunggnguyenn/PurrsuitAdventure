43ee01da47442362bf2e2a89fa148fe9c1053808
Phung Nguyen <phunggnguyenn@outlook.com>
2024-07-20 00:18:12 -0400

    Added images file and gameplay walkthrough

---
+++ images/cat.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=40x20 cat cat.png 
+ * Time-stamp: Thursday 07/18/2024, 23:14:12
+ * 
+ * Image Information
+ * -----------------
+ * cat.png 40@20
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef CAT_H
+#define CAT_H
+
+extern const unsigned short cat[800];
+#define CAT_SIZE 1600
+#define CAT_LENGTH 800
+#define CAT_WIDTH 40
+#define CAT_HEIGHT 20
+
+#endif
+
---
+++ images/instructions.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=240x160 instructions instructions.png 
+ * Time-stamp: Thursday 07/18/2024, 21:59:21
+ * 
+ * Image Information
+ * -----------------
+ * instructions.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+extern const unsigned short instructions[38400];
+#define INSTRUCTIONS_SIZE 76800
+#define INSTRUCTIONS_LENGTH 38400
+#define INSTRUCTIONS_WIDTH 240
+#define INSTRUCTIONS_HEIGHT 160
+
+#endif
+
